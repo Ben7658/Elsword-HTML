21af6e351d3cfc06789e2cf1f4a6fd23fbc53d57
Ben7658 <48234957+Ben7658@users.noreply.github.com>
2022-03-01 22:50:26 +0900

    Update Elsword-HTML.cpp

--- Elsword-HTML.cpp
+++ Elsword-HTML.cpp
@@ -17,8 +17,40 @@
 
     <h1>Table에 대하여</h1><br>
 
-    <table width="100%" border="1">
-        <caption>웹 표준교육과정</caption>
+    <table width="100%" border="1" summary="2022-02-28일부터 2022-03-01일 table에 대하여 공부함">
+        <caption>엘소드 마력작 옵션</caption>
+        <colgroup>
+            <col width="16%">
+            <col width="16%">
+            <col width="16%">
+            <col width="16%">
+            <col width="16%">
+            <col width="16%">
+        </colgroup>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>옵션</th>
+                <th>적색 마력석</th>
+                <th>청색 마력석</th>
+                <th>황색 마력석</th>
+                <th>거대 마력석</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th colspan="3">초록색 = 해당 색상에 높은 수치 효율을 볼 수 있는 옵션</th>
+                <th colspan="3">믿줄 = 해당 마력석으로 띄울 수 있는 극옵 수치</th>
+            </tr>
+        </tfoot>
+        <tbody>
+            <tr>
+                
+            </tr>
+            <tr>
+                
+            </tr>
+        </tbody>
     </table>
 
 
